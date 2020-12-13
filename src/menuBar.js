1fe0977d4af8a938611b7c5c0b7ad94718de8c09
foohooboo <danielmarshallevans@gmail.com>
2020-12-12 17:22:01 -0700

    done messing with menuBar for the night

--- src/menuBar.js
+++ src/menuBar.js
@@ -2,20 +2,14 @@ import React from 'react';
 import './menuBar.css';
 
 function MenuBar(props) {
-    // const menuItems = props.menuItems.map(item => {
-    //     <li><a href={item.address}>{item.name}</a></li>
-    // });
-
-    const menuItems = () => (
-        <div>
-            <ul>
-                {props.menuItems.map(item => <li key={item.name}><a href = {item.address}> {item.name} </a> </li>)}
-            </ul>
-        </div>
-    );
+    const menuItems = props.menuItems.map(item => {
+        <li><a href={item.address}>{item.name}</a></li>
+    });
 
     return(
+        <ul>
             {menuItems}
+        </ul>
     );
 }
 
