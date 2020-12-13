56b07b1b9833fb8ef7eeb71bdf3bc7f68d971c04
foohooboo <danielmarshallevans@gmail.com>
2020-12-12 17:03:46 -0700

    refactored MenuBar component

--- src/menuBar.js
+++ src/menuBar.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './menuBar.css';
 
 function MenuBar(props) {
-    const menuItems = props.menuItems.map(item => {
-        <li><a href={item.address}>{item.name}</a></li>
-    });
-    return(
+    // const menuItems = props.menuItems.map(item => {
+    //     <li><a href={item.address}>{item.name}</a></li>
+    // });
+
+    const menuItems = () => (
+        <div>
             <ul>
-                {menuItems}
+                {props.menuItems.map(item => <li key={name}><a href = {item.address}> {item.name} </a> </li>)}
             </ul>
+        </div>
+    );
+
+    return(
+            {menuItems}
     );
 }
 
