aa69a705eb83728974fbec2813bfe1cb658896cc
foohooboo <danielmarshallevans@gmail.com>
2020-12-14 16:20:03 -0700

    dropdown complete with hover effect and optional rightAlign argument

--- src/menuBar.js
+++ src/menuBar.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './menuBar.css';
 
+/**
+ * 
+ * @param {Array()} props Array of items to be displayed in a horizontal nav bar.
+ * props item format: {name: "someName", address: "someLink", rightAlign: boolean, dropDown: boolean}.
+ * rightAlign and dropDown are optional attributes
+ */
 function MenuBar(props) {
     const menuItems = props.menuItems.map(item => {
-        return (<li><a href={item.address}>{item.name}</a></li>);
+        if (item.rightAlign){
+            var additionalStyles = {float: "right"};
+        }
+        
+        return (<li style={additionalStyles} key={item.name}><a href={item.address}>{item.name}</a></li>);
     });
 
     return(
